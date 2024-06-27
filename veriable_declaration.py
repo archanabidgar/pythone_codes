fa979d90f31cb6d3fd7debd053500a57360cc429
archanabidgar <archanab010100@gmail.com>
2024-06-27 20:01:19 +0530

    veriable_declaration.py program

---
+++ veriable_declaration.py
@@ -0,0 +1,18 @@
+print("archana bidgar")
+x=5
+y="mona"
+print(x)
+print(y)
+num1 = 20
+name = "cdac"
+print(type(name))
+
+print(type(x))
+print(x)
+print(num1+x)
+#print(num1+name)
+"""so tired
+so angry
+so sleepy
+so hungry
+"""
